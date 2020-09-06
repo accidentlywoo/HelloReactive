da5b77021846ff7a8f24846819f1fce4818a540d
accidentlywoo <accidentlywoo@gmail.com>
2020-09-06 14:56:27 +0900

    ES6 Prototype

--- VanilaJS/Prototype.js
+++ VanilaJS/Prototype.js
@@ -18,3 +18,20 @@ var me = new Person('Woo');
 me.sayHi();
 
 console.log(me instanceof Person);
+
+// ES6
+// 클래스 선언문
+class Person2 {
+    // Constructor (생성자)
+    constructor(name){
+        this._name = name;
+    }
+    sayHi(){
+        console.log(`Hi ${this._name}`);
+    }
+}
+// 인스턴스 생성
+const me = new Person2('Woo');
+me.sayHi(); 
+
+console.log(me instanceof Person2);
