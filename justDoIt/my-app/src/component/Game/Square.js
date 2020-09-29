49aee26f1f83ece948c4ff5762b829b9728eaca7
maeve <maeve.woo@goodoc.co.kr>
2020-09-29 17:01:46 +0900

    state 관리의 시작 :

--- justDoIt/my-app/src/component/Game/Square.js
+++ justDoIt/my-app/src/component/Game/Square.js
@@ -1,6 +1,12 @@
 import React from 'react';
 
 class Square extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {// 컴ㅍㅎ넌트를 클릭한 것을 기억하게 만들어 X표시를 채워 넣자!
+            value:null,
+        };
+    }
     render(){
         return(
             <button className="square" onClick={() => {alert('click')}}>
