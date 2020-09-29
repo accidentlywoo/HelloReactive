18d2010ca7913f50d2cc34d8c6a133d93c3d2a97
maeve <maeve.woo@goodoc.co.kr>
2020-09-29 22:12:18 +0900

    sblings 통신, 부모 컴포넌트와 통신, 불변성

--- justDoIt/my-app/src/component/Game/Board.js
+++ justDoIt/my-app/src/component/Game/Board.js
@@ -2,8 +2,26 @@ import React from 'react';
 import Square from './Square';
 
 class Board extends React.Component {
-    renderSquare(i) {
-        return <Square value ={i} />
+    constructor(props){
+        // 자식놈들이 서로 통신하게하려면 부모 컴포넌트에 공유 state를 정의해야한다.
+        // -< 컴포넌트들이 서로 또는 부모 컴포넌트와 동기화 하도록 만든다.
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null),
+        };
+    }
+
+    handleClick(i){
+        const squares = this.state.squares.slice();// 원본 보호
+        squares[i] = 'x';
+        this.setState({squares: squares});
+    }
+
+    renderSquare(i) { 
+        return <Square 
+            value ={this.state.squares[i]}
+            onClick={() => this.handleClick(i)}     
+        />
     }
     render(){
         const status = 'Next player : X';
@@ -16,14 +34,14 @@ class Board extends React.Component {
                     {this.renderSquare(2)}
                 </div>
                 <div>
-                    {this.renderSquare(0)}
-                    {this.renderSquare(1)}
-                    {this.renderSquare(2)}
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
                 </div>
                 <div>
-                    {this.renderSquare(0)}
-                    {this.renderSquare(1)}
-                    {this.renderSquare(2)}
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
                 </div>
             </div>
         );
--- justDoIt/my-app/src/component/Game/Square.js
+++ justDoIt/my-app/src/component/Game/Square.js
@@ -9,7 +9,10 @@ class Square extends React.Component {
     }
     render(){
         return(
-            <button className="square" onClick={() => {alert('click')}}>
+            <button 
+                className="square" 
+                onClick={() => this.props.onClick()}
+            >
                 {this.props.value}
             </button>
         )
